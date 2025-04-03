2ff10e99e2eb68cdad6695964eb7899d28fde7c7
Hyerin Sung <hyerinx@naver.com>
2025-04-03 17:02:00 +0900

    🎨 import문 수정

--- Dfirstproject/community/views.py
+++ Dfirstproject/community/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from community.models import Community,Question
+from .models import Community, Question
 
 # Create your views here.
 
