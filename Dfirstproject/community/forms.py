86118d3ac69e69b0128c173a57d1eab9c9ca2f4c
zziminally <zziminally@gmail.com>
2025-04-15 15:58:56 +0900

    신지민: 250415

---
+++ Dfirstproject/community/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Post, Question, Comment
+
+class Postform(forms.ModelForm):
+    class Meta:
+        model=Post
+        fields=['title', 'content']
+
+class Commentform(forms.ModelForm):
+    class Meta:
+
+        model=Comment
+        fields=['username', 'comment_text']
+
+class Questionform(forms.ModelForm):
+    class Meta:
+        model=Question
+        fields=['title', 'content']
