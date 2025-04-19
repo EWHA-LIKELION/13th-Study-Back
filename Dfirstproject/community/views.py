a27d7183578ab43ac6fcbde612b1a7c4e4243c68
Hyerin Sung <hyerinx@naver.com>
2025-04-20 01:57:45 +0900

    🎨 views.py 주석 추가 및 코드 분류

--- Dfirstproject/community/views.py
+++ Dfirstproject/community/views.py
@@ -3,7 +3,9 @@ from django.utils import timezone
 from .models import Hashtag, Community, Question, Answer
 from .forms import QuestionForm, AnswerForm
 
-# Create your views here.
+# Community
+
+## Read
 
 def list(request):
     posts = Community.objects.filter(created_at__lte = timezone.now()).order_by('created_at')
@@ -12,6 +14,27 @@ def list(request):
 def detail(request, community_id):
     post = get_object_or_404(Community, pk=community_id)
     return render(request, 'detail.html', {'post':post})
+
+# Question
+
+## Create
+
+def post_question_create(request):
+    form = QuestionForm(request.POST, request.FILES)
+    if form.is_valid():
+        created_question = form.save(commit=False)
+        created_question.created_at = timezone.now()
+        created_question.save()
+
+        created_hashtags = [word for word in created_question.content.split() if word.startswith('#')]
+        for created_hashtag in created_hashtags:
+            hashtag = Hashtag.objects.get_or_create(hashtag = created_hashtag)
+            created_question.hashtag.add(hashtag[0])
+
+        return redirect('get_question_detail', created_question.id)
+    return redirect('get_question_list')
+
+## Read
 
 def get_question_list(request):
     query_string_상태 = request.GET.get('상태')
@@ -35,26 +58,13 @@ def get_question_detail(request, question_id):
 def get_question_create(request):
     form = QuestionForm()
     return render(request, 'question_create.html', {'form':form})
-    
-def post_question_create(request):
-    form = QuestionForm(request.POST, request.FILES)
-    if form.is_valid():
-        created_question = form.save(commit=False)
-        created_question.created_at = timezone.now()
-        created_question.save()
-
-        created_hashtags = [word for word in created_question.content.split() if word.startswith('#')]
-        for created_hashtag in created_hashtags:
-            hashtag = Hashtag.objects.get_or_create(hashtag = created_hashtag)
-            created_question.hashtag.add(hashtag[0])
-
-        return redirect('get_question_detail', created_question.id)
-    return redirect('get_question_list')
 
 def get_question_update(request, question_id):
     form = QuestionForm()
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'question_update.html', {'form':form, 'question':question})
+
+## Update
 
 def post_question_update(request, question_id):
     prev_question = get_object_or_404(Question, pk=question_id)
@@ -72,10 +82,16 @@ def post_question_update(request, question_id):
         return redirect('get_question_detail', updated_question.id)
     return redirect('get_question_list')
 
+## Delete
+
 def delete_question(requset, question_id):
     question = get_object_or_404(Question, pk=question_id)
     question.delete()
     return redirect('get_question_list')
+
+# Answer
+
+## Create
 
 def post_answer_create(request):
     question_id = request.GET.get('question_id')
@@ -89,6 +105,8 @@ def post_answer_create(request):
         return redirect('get_question_detail', created_answer.question.id)
     return redirect('get_question_detail', question.id)
 
+## Update
+
 def post_answer_update(request, answer_id):
     prev_answer = get_object_or_404(Answer, pk=answer_id)
     form = AnswerForm(request.POST, instance=prev_answer)
@@ -97,6 +115,8 @@ def post_answer_update(request, answer_id):
         updated_answer.save()
         return redirect('get_question_detail', updated_answer.question.id)
     return redirect('get_question_detail', prev_answer.question.id)
+
+## Delete
 
 def delete_answer(request, answer_id):
     answer = get_object_or_404(Answer, pk=answer_id)
