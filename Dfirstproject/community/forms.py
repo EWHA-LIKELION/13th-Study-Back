3fed1b441c745c21f4062026af99056f84dea01e
Hyerin Sung <hyerinx@naver.com>
2025-05-13 20:22:44 +0900

    🎨 코드 포맷팅

--- Dfirstproject/community/forms.py
+++ Dfirstproject/community/forms.py
@@ -4,9 +4,9 @@ from .models import Question, Answer
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
-        fields = ['title', 'content', 'username', 'status']
+        fields = ['status','title','username','content']
 
 class AnswerForm(forms.ModelForm):
     class Meta:
         model = Answer
-        fields = ['content', 'username']
+        fields = ['content','username']
