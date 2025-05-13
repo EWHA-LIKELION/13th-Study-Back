f6ad5872f363c1daab99aa5eb4f5e7fbdf5bb2a4
Hyerin Sung <hyerinx@naver.com>
2025-05-13 19:59:07 +0900

    🔥 불필요한 주석 삭제

--- Dfirstproject/community/admin.py
+++ Dfirstproject/community/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from community.models import User, Hashtag, Community, Question, Answer, LikeQuestion, LikeAnswer
 
-# Register your models here.
-
 admin.site.register(User)
 admin.site.register(Hashtag)
 admin.site.register(Community)
