2bb8e87e052eb393e6a7d3080e06465e44a3f8ae
gyuuuuri <gyuri113@ewha.ac.kr>
2025-05-22 16:58:47 +0900

    DRF 수정

--- LionDRF/blog/admin.py
+++ LionDRF/blog/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from .models import *
-
+from blog.models import *
 
 # Register your models here.
-
-admin.site.register(CustomUser)
-
+admin.site.register(Post)
 
