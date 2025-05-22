22ec91d614cfc462e94876a8b9b89be16cd71cc0
Hyerin Sung <hyerinx@naver.com>
2025-05-22 19:07:36 +0900

    🔨 Hashtag, Community, Question, Answer, LikeQuestion, LikeAnswer 모델 추가

--- DRF/forums/admin.py
+++ DRF/forums/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Hashtag, Community, Question, Answer, LikeQuestion, LikeAnswer
 
-# Register your models here.
+admin.site.register(Hashtag)
+admin.site.register(Community)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(LikeQuestion)
+admin.site.register(LikeAnswer)
--- DRF/forums/models.py
+++ DRF/forums/models.py
@@ -1,3 +1,98 @@
 from django.db import models
+from django.utils import timezone
+from accounts.models import User
 
-# Create your models here.
+class Hashtag(models.Model):
+    hashtag = models.CharField()
+
+    def __str__(self):
+        return self.hashtag
+
+class Post(models.Model):
+    writer = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(
+        default=timezone.now,
+    )
+    title = models.CharField(
+        max_length=50,
+    )
+    content = models.TextField()
+    hashtag = models.ManyToManyField(
+        Hashtag,
+        null=True,
+        blank=True,
+    )
+    image = models.ImageField(
+        upload_to='post/image',
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        abstract = True
+
+class Community(Post):
+    pass
+
+class Question(Post):
+    status = models.BooleanField()
+
+class Answer(models.Model):
+    writer = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(
+        default=timezone.now,
+    )
+    question = models.ForeignKey(
+        Question,
+        related_name='answers',
+        on_delete=models.CASCADE,
+    )
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
+    
+class LikeQuestion(models.Model):
+    created_at = models.DateTimeField(
+        default=timezone.now,
+    )
+    question = models.ForeignKey(
+        Question,
+        related_name='likes',
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        User,
+        related_name='like_question',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f'{self.user.first_name} 님이 "{self.question.title}" 질문을 좋아합니다.'
+    
+class LikeAnswer(models.Model):
+    created_at = models.DateTimeField(
+        default=timezone.now,
+    )
+    answer = models.ForeignKey(
+        Answer,
+        related_name='likes',
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        User,
+        related_name='like_answer',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f'{self.user.first_name} 님이 "{self.answer.content}" 답변을 좋아합니다.'
