3efa63b13600b89f654f2661decb6feb4da13d92
Hyerin Sung <hyerinx@naver.com>
2025-05-23 01:39:36 +0900

    🔨 forums 앱 views.py import문 작성

--- DRF/forums/views.py
+++ DRF/forums/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from django.http import Http404
+from rest_framework import views
+from rest_framework import status
+from rest_framework.response import Response
+from accounts.models import User
+from .models import Hashtag, Community, Question, Answer, LikeQuestion, LikeAnswer
+from .serializers import CommunitySerializer, QuestionSerializer, AnswerSerializer
 
-# Create your views here.
