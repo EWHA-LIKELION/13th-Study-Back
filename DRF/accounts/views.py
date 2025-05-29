1582756cbd6a95175b6ad32a316964e7e4d4ab22
Hyerin Sung <hyerinx@naver.com>
2025-05-29 18:03:01 +0900

    🔨 accounts 앱 Login, UserRoot 뷰 추가

--- DRF/accounts/views.py
+++ DRF/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer, LoginSerializer
 
-# Create your views here.
+class Login(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserRoot(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
