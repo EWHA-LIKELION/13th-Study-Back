1e9e174ebcdf129bf113bff6d88d4cd73d75ff6d
zziminally <zziminally@gmail.com>
2025-06-03 01:32:30 +0900

    신지민: 250529

--- LionDRF/blog/urls.py
+++ LionDRF/blog/urls.py
@@ -5,6 +5,11 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', PostList.as_view()),
-    path('<int:pk>/', PostDetail.as_view()),
-    path('create/', PostList.as_view())
+    path('posts/<int:pk>/', PostDetail.as_view()),
+    path('create/', PostList.as_view()),
+    path('comments/', CommentView.as_view()),
+    path('comments/<int:pk>/', CommentDetail.as_view()),
+
+    path('posts/<int:pk>/like/', PostLike.as_view()),
+    path('comments/<int:pk>/like/', CommentLike.as_view()),
 ]
