da276a6b782670165c821a178bac842908f7ba42
Hyerin Sung <hyerinx@naver.com>
2025-06-24 09:46:45 +0900

    🔨 accounts 앱 User 뷰 get, put, delete 추가

--- DRF/accounts/views.py
+++ DRF/accounts/views.py
@@ -12,6 +12,10 @@ class Login(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class User(APIView):
+    def get(self, request, format=None):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def post(self, request, format=None):
         serializer = UserSerializer(data=request.data)
 
@@ -19,3 +23,15 @@ class User(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, format=None):
+        serializer = UserSerializer(request.user, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, format=None):
+        request.user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
